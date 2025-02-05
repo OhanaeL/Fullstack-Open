86d237afcb2ce340f708e55e050ff9f6cf997ad3
Ohanael <htinlinn.116900@gmail.com>
2025-02-06 04:25:21 +0700

    done for ex 2-11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,16 +1,21 @@
-// App.js
-import { useState } from 'react';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import FilterTextBox from './filterBox';
 import PersonList from './displayList';
 import AddPerson from './addPerson';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+
   const [formData, setFormData] = useState({
     name: '',
     number: '',
